655b33845d0f1109d1dd2c4d540e6fca1f337907
lior2 <lior@meteor-comm.com>
2017-05-01 14:29:57 +0300

    port number changed to 90

--- server_example/server.js
+++ server_example/server.js
@@ -54,6 +54,6 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
 });
 
 //listen on port 8080
-webServer.listen(8080, function () {
-    console.log('listening on http://localhost:8080');
+webServer.listen(90, function () {
+    console.log('listening on http://localhost:90');
 });
